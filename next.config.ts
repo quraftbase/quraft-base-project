ade6b8e09ccb1e7c6490f58c964263ee08363fae
quraftbase <veryhappyman10@gmail.com>
2025-12-25 13:41:02 +0900

    disable eslint and ts check during build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+
   eslint: {
     ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
